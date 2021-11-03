453e3448b745cc0a7875c6280ace850a69790c33
Etomia <Emiliene_@hotmail.com>
2021-11-03 15:02:52 +0100

    kommet videre med js

--- Scripting.js
+++ Scripting.js
@@ -1,3 +1,31 @@
-function startTest(){
+const timer = document.getElementById('#timer');
 
+
+let stoptime = true;
+let sec = 0;
+let min = 0;
+
+function startTest(){
+    document.getElementById('timer').innerHTML = "000000000000000000000000";
+    if(stoptime){
+        stoptime = false;
+        timercycle();
+    }
+}
+
+function timercycle(){
+    if(stoptime){
+        return;
+    }
+    sec = parseInt(sec);
+
+
+    if(sec === 60 ){
+        min ++;
+        sec = 0;
+    }
+
+    document.getElementById('timer').innerHTML = min + " : " + sec;
+
+    timercycle();
 }
